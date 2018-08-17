a9b6e0db9666a377886e994ffbabfa0567f03359
Your Name <you@example.com>
2018-08-17 09:40:37 +0100

    my smart_brain project

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'fishes1990',
+      database : 'smart_brain'
+    }
+  });
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => { res.send(database.users); })
+ 
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', (req , res) => {
+    register.handleRegister(req, res, db, bcrypt)} )
+
+app.get('/profile/:id', (req, res) => {
+    profile.handleProfile(req, res, db)
+});
+
+app.put('/image', (req, res) => {
+    image.handleImage(req, res, db)
+});
+app.post('/imageurl', (req, res) => {
+    image.handleApiCall(req, res)
+});
+
+//bcrypt.hash("bacon", null, null, function(err, hash){
+    //store hash in your password DB
+//});
+
+//load hash from your password DB
+//bcrypt.compare("bacon", hash, function(err, res){
+      //res === true
+//});
+//bcrypt.compare("veggies", hash, function(err, res){
+    //res === false
+//});
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
